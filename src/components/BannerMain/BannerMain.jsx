57b6bb30469e6803e23eacbf94da3d65f836aa19
Matias Godoy <matiasgodoy7@hotmail.com>
2022-03-10 22:51:51 -0300

    more improvements in main banner

--- src/components/BannerMain/BannerMain.jsx
+++ src/components/BannerMain/BannerMain.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./BannerMain.css"
-import ImageLogo from "../assets/matiWebDevLogo.png"
+import imageLogo from "../assets/blackLogo.png"
 import '@fontsource/roboto/300.css';
 import Typography from '@mui/material/Typography';
 
@@ -11,9 +11,9 @@ const BannerMain = () => {
         <div className='containerBanner'>
             <div className='containerBanner-LogoAnimation' >
                 <div>
-                    <img src={ImageLogo} className="imgLogoBanner"/>
+                    <img src={imageLogo} className="imgLogoBanner"/>
                 </div>
-                <div>
+                <div className='containerBanner-Typo'>
                     <Typography variant="h2" gutterBottom component="div" className='animate__animated animate__backInDown animate__slow'>
                         Bienvenido
                     </Typography>
@@ -22,9 +22,11 @@ const BannerMain = () => {
                     </Typography>
                 </div>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='wave'>
-                <path fill="#7B2CBF" fill-opacity="1" d="M0,96L48,117.3C96,139,192,181,288,186.7C384,192,480,160,576,154.7C672,149,768,171,864,181.3C960,192,1056,192,1152,176C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            <div className='wave'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#C9ADA7" fill-opacity="1" d="M0,96L48,117.3C96,139,192,181,288,186.7C384,192,480,160,576,154.7C672,149,768,171,864,181.3C960,192,1056,192,1152,176C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
             </svg>
+            </div>
         </div>
     )
 }
